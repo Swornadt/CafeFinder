38cd0abcbd6df3a4eb8492b90585235937e840e7
swornadt <tuladharsworna@gmail.com>
2025-09-29 16:26:15 +0545

    refactored for deployment v2

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import cafeRoutes from "./routes/cafeRoutes.js";
 import geocodeRoutes from "./routes/geocodeRoutes.js";
 
@@ -9,6 +10,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Serve static frontend
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
 app.get("/test", (req, res) => {
     res.send("Test Pass")
 })
@@ -16,6 +21,10 @@ app.get("/test", (req, res) => {
 app.use("/api/cafes", cafeRoutes);
 app.use("/api/geocode", geocodeRoutes);
 
+// Catch-all to serve index.html for React routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
